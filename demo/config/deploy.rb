7c6e7cd8e95526630b1c1842ef961e3bd31ba440
codedogfish <jackcool1989@outlook.com>
2015-09-24 14:55:56 +0800

    Change cap deploy

--- demo/config/deploy.rb
+++ demo/config/deploy.rb
@@ -1,8 +1,6 @@
 require 'bundler/capistrano'     #添加之后部署时会调用bundle install， 如果不需要就可以注释掉
-#require "capistrano/ext/multistage"     #多stage部署所需
-#set :stages, %w(development production)
-#set :default_stage, "development"
 set :application, "rails-in-action"
+set :app_port, "10000"
 set :repository,  "http://github.com/codedogfish/rails-in-action.git"
 set :keep_releases, 5          #只保留5个备份
 set :deploy_to, "/home/jackyu/#{application}"  #部署到远程机器的路径
@@ -14,8 +12,6 @@ set :use_sudo, false                            #执行的命令中含有sudo，
 set :runner, "jackyu"
 set :git_username, "codedogfish"
 set :scm, :git
-# set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
 role :web, "server02"                          # Your HTTP server, Apache/etc
 role :app, "server02"                          # This may be the same as your `Web` server
@@ -40,9 +36,11 @@ namespace :deploy do
     task :start do ; end
     task :stop do ; end
     task :restart, :roles => :app, :except => { :no_release => true } do
-        run "pid=`lsof -n -i:10000|grep TCP|grep LISTEN|grep IPv4|awk '{printf(\"%d\\n\"),$2}'` && echo $pid > #{deploy_to}/demo/pid"
-        run "kill $pid"
-        run "cd #{deploy_to}/demo && rails s -p 10000 -d"
+        #run "lsof -n -i:10000|grep TCP|grep LISTEN|grep IPv4|head -1|awk '{print $2}'"
+        #run "lsof -n -i:#{port}|grep TCP|grep LISTEN|grep IPv4|head -1|awk '{print $2}'>#{deploy_to}/shared/pids/demo"
+        run "kill -KILL $(cat #{deploy_to}/shared/pids/server.pid)"
+        run "cd #{deploy_to}/current/ && rails s -p #{app_port} -d"
+        #run "sleep 2 && lsof -n -i:#{app_port}|grep TCP|grep LISTEN|grep IPv4|head -1|awk '{print $2}'>#{deploy_to}/shared/pids/#{application}"
     end
 
     desc "Checkout subdirectory and delete all the other stuff"
@@ -50,7 +48,8 @@ namespace :deploy do
         run "mv #{current_release}/#{deploy_subdir}/ /tmp && rm -rf #{current_release}/* && mv /tmp/#{deploy_subdir}/* #{current_release}"
     end
 end
-puts "register before"
-before "deploy:finalize_update" do
-    run "mv #{current_release}/#{deploy_subdir}/ /tmp && rm -rf #{current_release}/* && mv /tmp/#{deploy_subdir}/* #{current_release}"
-end
+#puts "register before"
+#before "deploy:finalize_update" do
+#    run "mv #{current_release}/#{deploy_subdir}/ /tmp && rm -rf #{current_release}/* && mv /tmp/#{deploy_subdir}/* #{current_release}"
+#end
+
