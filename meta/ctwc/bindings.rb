92056c0ef7834039727ec20977a1a0d1e0b10f77
codedogfish <jackcool1989@outlook.com>
2015-10-17 10:26:49 +0800

    Practice on chapter 5.2 Kernel#eval TOPLEVEL_BINDING

--- meta/ctwc/bindings.rb
+++ meta/ctwc/bindings.rb
@@ -16,3 +16,15 @@ b = MyClass.new.my_method
 eval "@x+=1", b
 eval "puts @x", b
 eval "puts @y", b
+eval "puts self.inspect", b
+
+class AnotherClass
+    def my_method
+        eval "puts self.inspect", TOPLEVEL_BINDING
+    end
+end
+
+@z = 3
+
+AnotherClass.new.my_method
+puts inspect
